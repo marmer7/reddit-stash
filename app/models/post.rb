dd364ba24bff9c4bcccdcc3bcacccf4fc052f560
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-23 22:23:39 -0400

    reddit adapter

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post
+
+  def initialize
+    @client = RedditAdapter.new
+  end
+
+  
+
+end
--- app/models/reddit_adapter.rb
+++ app/models/reddit_adapter.rb
@@ -3,15 +3,46 @@ require 'rest-client'
 
 class RedditAdapter
 
-  attr_accessor :link
-
-  def initialize(link = "https://www.reddit.com/r/all.json")
-    @link = link
-  end
+  attr_accessor :link, :current_subreddit, :page, :count
 
   def request
+    # reddit hash from JSON
     @request = JSON.parse(RestClient.get(self.link))
   end
+
+  def initialize(page = 0, subreddit = "all")
+    @page = page
+    @current_subreddit = subreddit
+    self.set_up
+  end
+
+  def set_up
+    binding.pry
+    @count = @page * 25
+    @link = "https://www.reddit.com/r/#{current_subreddit}/.json?count=#{count}"
+    self.request
+  end
+
+  def next_page
+    @page += 1
+    self.set_up
+  end
+
+  def last_page
+    @page -= 1
+    self.set_up
+  end
+
+  def change_subreddit(subreddit)
+    @current_subreddit = subreddit
+    self.set_up
+  end
+
+  def posts
+    self.request["data"]["children"]
+  end
+
+  # title request["data"]["children"][0]["data"]["title"]
 
 end
 
